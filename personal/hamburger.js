1bbceba51315d5e931ed7bcb2e17d0440a1dd916
Avee1000 <eddyidahosa01@gmail.com>
2023-12-16 13:57:56 +0100

    personal

--- personal/hamburger.js
+++ personal/hamburger.js
@@ -1,9 +1,13 @@
 const hidden = document.getElementById("hidden-links");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    if (hidden.style.display == "none"){
-        hidden.style.display = "block";
-    }else {
-        hidden.style.display = "none";
-    }
+
+hamburgerMenu.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent the click event from reaching the window click listener
+    hidden.style.display = (hidden.style.display === "none") ? "block" : "none";
+});
+
+// Add a click event listener to the window to close the element when clicking outside
+window.addEventListener("click", () => {
+    hidden.style.display = "none";
 });
