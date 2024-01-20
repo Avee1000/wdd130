cd84859d02c4870ee1093f94cbe47e6790a6c3ea
Avee1000 <eddyidahosa01@gmail.com>
2024-01-20 14:29:36 +0100

    sgsw

--- personal/hamburger.js
+++ personal/hamburger.js
@@ -1,8 +1,7 @@
 const hidden = document.getElementById("hidden-links");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-
-hamburgerMenu.addEventListener("click", (event) => {
+hamburgerMenu.addEventListener("click", () => {
     event.stopPropagation(); // Prevent the click event from reaching the window click listener
     hidden.style.display = (hidden.style.display === "none") ? "block" : "none";
 });
@@ -10,4 +9,11 @@ hamburgerMenu.addEventListener("click", (event) => {
 // Add a click event listener to the window to close the element when clicking outside
 window.addEventListener("click", () => {
     hidden.style.display = "none";
+});
+
+// JavaScript to scroll to the top of the page when the "Back to top" link is clicked:
+const scrollToTop = document.querySelector(".accordion");
+
+scrollToTop.addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
 });
